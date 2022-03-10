b411c610f7b04626f4a3451c66ab52261cd23e9a
flowsinator~ <florezalbeiro87@gmail.com>
2022-03-09 19:13:03 -0500

    created the first loop for weekly expenses

--- index.js
+++ index.js
@@ -3,4 +3,7 @@ let monthlyExpenses = 0
 let annualExpenses = 0
 
 // Your Code Here
-window.prompt(weeklyExpenseQuestions[0])
+for(let i =0; i <weeklyExpenseQuestions.lenght; i++){
+    let answer = parseFloat(window.prompt(weeklyExpenseQuestions[i]))
+    weeklyExpenses = weeklyExpenses + answer
+}
