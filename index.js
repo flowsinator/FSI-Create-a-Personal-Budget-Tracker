914ae9d94a262ae5edf5eb46965ed53c0615c521
flowsinator~ <florezalbeiro87@gmail.com>
2022-03-09 19:17:17 -0500

    Added all the loops as requested

--- index.js
+++ index.js
@@ -7,3 +7,13 @@ for(let i = 0; i < weeklyExpenseQuestions.length; i++){
     let answer = parseFloat(window.prompt(weeklyExpenseQuestions[i]))
     weeklyExpenses = weeklyExpenses + answer
 }
+
+for(let i = 0; i < monthlyExpenseQuestions.length; i++){
+    let answer = parseFloat(window.prompt(monthlyExpenseQuestions[i]))
+    monthlyExpenses = monthlyExpenses + answer
+}
+
+for(let i = 0; i < annualExpenseQuestions.length; i++){
+    let answer = parseFloat(window.prompt(annualExpenseQuestions[i]))
+    annualExpenseQuestions = annualExpenses + answer
+}
