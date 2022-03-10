51df4de68f12d5b9e303e473b862a925d1c32779
flowsinator~ <florezalbeiro87@gmail.com>
2022-03-09 19:14:47 -0500

    corrected issues in code

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ let monthlyExpenses = 0
 let annualExpenses = 0
 
 // Your Code Here
-for(let i =0; i <weeklyExpenseQuestions.lenght; i++){
+for(let i = 0; i < weeklyExpenseQuestions.length; i++){
     let answer = parseFloat(window.prompt(weeklyExpenseQuestions[i]))
     weeklyExpenses = weeklyExpenses + answer
 }
