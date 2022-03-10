e765caf92fc4870d7662b2ef19a54b7f512b7e6d
flowsinator~ <florezalbeiro87@gmail.com>
2022-03-09 19:22:40 -0500

    Change my prompts to alerts

--- index.js
+++ index.js
@@ -3,27 +3,27 @@ let monthlyExpenses = 0
 let annualExpenses = 0
 
 // Your Code Here
-window.prompt(`You are about to be asked questions about your weekly expenses`)
+window.alert(`You are about to be asked questions about your weekly expenses`)
 
 for(let i = 0; i < weeklyExpenseQuestions.length; i++){
     let answer = parseFloat(window.prompt(weeklyExpenseQuestions[i]))
     weeklyExpenses = weeklyExpenses + answer
 }
 
-window.prompt(`Thank you for that`)
-window.prompt(`Now, you are about to be asked questions about your monthly expenses`)
+window.alert(`Thank you for that`)
+window.alert(`Now, you are about to be asked questions about your monthly expenses`)
 
 for(let i = 0; i < monthlyExpenseQuestions.length; i++){
     let answer = parseFloat(window.prompt(monthlyExpenseQuestions[i]))
     monthlyExpenses = monthlyExpenses + answer
 }
 
-window.prompt(`Thank you for that`)
-window.prompt(`Finally, you are about to be asked questions about your annual expenses`)
+window.alert(`Thank you for that`)
+window.alert(`Finally, you are about to be asked questions about your annual expenses`)
 
 for(let i = 0; i < annualExpenseQuestions.length; i++){
     let answer = parseFloat(window.prompt(annualExpenseQuestions[i]))
     annualExpenses = annualExpenses + answer
 }
 
-window.prompt(`Thank you for that, now review your budget below! Have a great day`)
+window.alert(`Thank you for that, now review your budget below! Have a great day`)
